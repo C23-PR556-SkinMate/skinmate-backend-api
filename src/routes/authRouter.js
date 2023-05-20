675535949c65b34edba6eb2dfd85dabe3dae541f
hsymicm <ichorvfx@gmail.com>
2023-05-20 16:02:24 +0700

    Folder rename

--- src/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authRateLimitter } = require('../middleware/limiterMiddleware');
-const { login, register } = require('../controller/authController');
-
-router.post('/login', authRateLimitter, login);
-router.post('/register', authRateLimitter, register);
-
-module.exports = router;
