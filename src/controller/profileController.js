7b327c7e5e4f0db44e9a3d1c6dab4bea843a85d4
hsymicm <ichorvfx@gmail.com>
2023-05-20 16:01:43 +0700

    Added set profile img function & modified set profile function

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -1,18 +1,79 @@
 const app = require('../firebase');
+const { validateImageType } = require('../helper/imageValidationHelper');
+const uploadImage = require('../helper/uploadImageHelper');
 const db = app.firestore();
 require('dotenv').config();
 
 const template = {
+    profileImg: null,
     displayName: '',
     description: '',
-    birthday: '',
-    profileImg: '',
-    hobby: '',
+    dateOfBirth: '',
+    skinType: '',
+    gender: '',
+    reminderDay: '',
+    reminderNight: '',
+};
+
+const getProfile = async (req, res) => {
+    const { uid } = req.params;
+    
+    try {
+        const profileRef = db.collection('profiles');
+        const profileSnapshot = await profileRef.doc(uid).get();
+        
+        if (profileSnapshot.exists) {
+            const { 
+                profileImg,
+                displayName,
+                description,
+                dateOfBirth,
+                skinType,
+                gender,
+                reminderDay,
+                reminderNight,
+            } = profileSnapshot.data();
+
+            return res.status(200).json({
+                data: {
+                    profileImg,
+                    displayName,
+                    description,
+                    dateOfBirth,
+                    skinType,
+                    gender,
+                    reminderDay,
+                    reminderNight,
+                },
+                message: 'Successfully found the user profile',
+                success: true,
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Error, cannot find the user profile',
+            });
+        }
+    } catch (error) {
+        res.status(500);
+    }
 };
 
 const setProfile = async (req, res) => {
-    const uid = req.uid;
+    const { uid, email } = req.result;
     const { displayName, description } = req.body;
+    const compareUid = req.body.uid;
+
+    if (!compareUid) {
+        return res.status(404).json({
+            message: 'Error, missing uid',
+        });
+    }
+
+    if (uid !== compareUid) {
+        return res.status(404).json({
+            message: 'Error, invalid uid'
+        });
+    }
 
     try {
         const profileRef = db.collection('profiles');
@@ -20,10 +81,12 @@ const setProfile = async (req, res) => {
         
         await profileRef.doc(uid).set({
             ...template,
-            displayName,
+            displayName: displayName || email.split['@'][0],
             description,
             createdAt,
             updatedAt : createdAt,
+        }, {
+            merge: true,
         });
         
         return res.status(200).json({
@@ -31,42 +94,67 @@ const setProfile = async (req, res) => {
             success: true,
         });
     } catch (error) {
-        console.log(error);
+        res.status(500);
     }
-    
-    res.status(500);
 };
 
-const getProfile = async (req, res) => {
-    const { uid } = req.params;
+const setProfilePicture = async (req, res) => {
+    const { uid } = req.result;
+    const file = req.file;
+    const compareUid = req.body.uid;
 
-    try {
-        const profileRef = db.collection('profiles');
-        const profileSnapshot = await profileRef.doc(uid).get();
-        
-        if (profileSnapshot.exists) {
-            const { displayName, description } = profileSnapshot.data();
-            return res.status(200).json({
-                data: {
-                    displayName,
-                    description,
-                },
-                message: 'Successfully found the user profile',
-                success: true,
-            });
-        } else {
-            return res.status(404).json({
-                message: 'Cannot find the user profile',
-            });
-        }
-    } catch (error) {
-        console.log(error);
+    if (!compareUid) {
+        return res.status(404).json({
+            message: 'Error, missing uid',
+        });
     }
-    
-    res.status(500);
+
+    if (uid !== compareUid) {
+        return res.status(404).json({
+            message: 'Error, invalid uid'
+        });
+    }
+
+    if (!file) {
+        return res.status(404).json({
+            message: 'Error, missing file',
+        });
+    }
+
+    if (!validateImageType(file)) {
+        return res.status(404).json({
+            message: 'Error, invalid file type',
+        });
+    }
+
+    try {    
+        const url = await uploadImage(file);
+        
+        const profileRef = db.collection('profiles');
+        const updatedAt = Date.now();
+
+        await profileRef.doc(uid).set({
+            profileImg: url,
+            updatedAt
+        }, {
+            merge: true
+        });
+
+        return res.status(200).json({
+            data: {
+                uid,
+                profileImg: url,
+            },
+            message: 'Profile image has been successfully uploaded',
+            success: true,
+        });
+    } catch (error) {
+        res.status(500);
+    }
 };
 
 module.exports = {
     setProfile,
     getProfile,
+    setProfilePicture,
 };
