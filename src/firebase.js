44af91f3f9ea15ded68087bd0d4a8c422b108743
hsymicm <ichorvfx@gmail.com>
2023-05-20 16:00:14 +0700

    Added storage bucket config

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../service-account.json');
 
 const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'sandbox-bucket-101'
 });
 
 module.exports = app;
