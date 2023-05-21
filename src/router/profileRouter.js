f0cdc9bb90ae794648268e08fa363c2ff2d183f1
hsymicm <ichorvfx@gmail.com>
2023-05-21 19:59:23 +0700

    Added update profile routes

--- src/router/profileRouter.js
+++ src/router/profileRouter.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const multerMiddleware = require('../middleware/multerMiddleware');
 const { verifyToken } = require('../middleware/tokenMiddleware');
-const { setProfile, getProfile, setProfilePicture } = require('../controller/profileController');
+const {
+    updateProfile,
+    getProfile,
+    setProfilePicture,
+} = require('../controller/profileController');
 
-router.post('/profile', verifyToken, setProfile);
 router.post('/profile/upload', verifyToken, multerMiddleware.single('file'), setProfilePicture);
 router.get('/profile/:uid', verifyToken, getProfile);
+router.put('/profile/:uid', verifyToken, updateProfile);
 
 module.exports = router;
