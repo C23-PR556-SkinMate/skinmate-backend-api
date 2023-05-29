8eec38384d83d59b8146017e119b5e46ca1bd158
dvnazzra <devianaazzahra4@gmail.com>
2023-05-29 16:18:58 +0700

    update config

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../service-account.json');
+const serviceAccount = require('../config/service-account.json');
 
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
