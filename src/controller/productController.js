e619b7e81bf6e255eda86877f3f3b77185cbb61e
hsymicm <ichorvfx@gmail.com>
2023-06-09 13:13:16 +0700

    Added Product Controller

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,17 +1,43 @@
-const { skinProducts, skinIssues } =  require('../data/products');
+const app = require('../firebase');
+const db = app.firestore();
+
+const validProblems = ['acnes', 'blackheads', 'darkspot', 'wrinkles'];
+const validSkinTypes = ['normal', 'dry', 'oily', 'combination'];
+const direction = ['asc', 'desc'];
+const defaultLimit = 5;
+
+const shuffleArray = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+};
+
+const fetchDb = async (arr, collection, tag, limit) => {
+    const tagRef = db.collection(collection);
+    const tagSnapshot = await tagRef
+        .where('tags', '==', tag)
+        .where('tags', )
+        .orderBy('__name__', direction[Math.round(Math.random())])
+        .limit(Number(limit) || defaultLimit)
+        .get();
+
+    if (!tagSnapshot.empty) {
+        tagSnapshot.docs.forEach((doc) => {
+            const data = doc.data();
+            data.price = 'Rp ' + data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+            arr.push(data);
+        });
+        shuffleArray(arr);
+    }
+};
 
 const getProducts = async (req, res, next) => {
-    const { tag, limit } = req.query;
+    const { problem, skintype, limit } = req.query;
 
-    if (!tag) {
+    if (!problem && !skintype) {
         return res.status(404).json({
-            message: 'Error, missing tag'
-        });
-    }
-
-    if (!skinIssues.includes(tag)) {
-        return res.status(404).json({
-            message: 'Error, tag does not exists'
+            message: 'Error, missing problem or skintype query'
         });
     }
 
@@ -21,20 +47,37 @@ const getProducts = async (req, res, next) => {
         });
     }
 
+    if (problem && !validProblems.includes(problem)) {
+        return res.status(404).json({
+            message: 'Error, invalid problem query'
+        });
+    }
+
+    if (skintype && !validSkinTypes.includes(skintype)) {
+        return res.status(404).json({
+            message: 'Error, invalid skintype query'
+        });
+    }
+
     try {
-        const filteredProducts = skinProducts.filter((product) => {
-            return product.tags.includes(tag);
-        }).splice(0, limit || 5);
+        const skinTreatment = [];
+        const dailyCare = [];
+
+        if (problem) await fetchDb(skinTreatment, 'skinTreatment', problem, limit);
+        if (skintype) await fetchDb(dailyCare, 'dailyCare', skintype, limit);
 
         res.status(200).json({
             data: {
-                tag,
-                products: filteredProducts,
+                products: [
+                    ...skinTreatment,
+                    ...dailyCare,
+                ],
             },
             message: 'Successfully retrieved the products',
             success: true,
         });
     } catch (error) {
+        console.log(error);
         next(error);
     }
 };
